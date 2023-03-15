f63a3599809681b0e78c3ed541cdac5125c1bdc0
Mustafa-Alhnuaiti <83397235+Mustafa-AlHunaiti@users.noreply.github.com>
2023-03-15 07:01:33 -0700

    Update Group By + Order By Statements.sql

--- Group By + Order By Statements.sql
+++ Group By + Order By Statements.sql
@@ -1,9 +1,24 @@
 /*
 Group By, Order By
 */
--- 
+-- Group by statement is similar to distinct in the select statement the difference is if we say 
+
 SELECT DISTINCT(Gender)
 FROM EmployeeDemographics
+-- return the very first unique value of female and the very first unique value of male
+
+-- but if we say
+SELECT Gender 
+FROM EmployeeDemographics
+GROUP BY Gender
+--it's only gonna return two values but in this two values we actually have all the males rolled up into this one row "Male" and all the females rolled up into this one row "Female"
+-- let me further show you what that means
+SELECT Gender, count(Gender) AS CountGender 
+FROM EmployeeDemographics
+GROUP BY Gender
+-- now you can see that this whole time there were six males in this one row  "Male" and there were three females in this one row "Female"
+
+
 
 SELECT Gender, count(Gender) AS CountGender 
 FROM EmployeeDemographics
