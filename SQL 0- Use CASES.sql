79274e726774cdfed1c1e84d8cd87092ab6fa663
Mustafa-Alhnuaiti <83397235+Mustafa-AlHunaiti@users.noreply.github.com>
2023-03-17 02:46:45 +0300

    Update and rename SQL 0- Use CASES to SQL 0- Use CASES.sql

--- SQL 0- Use CASES.sql
+++ SQL 0- Use CASES.sql
@@ -1,8 +1,12 @@
 
 
 ----------------------------------- USE CASES ------------------------------------------------------
-
-
+/*
+Robert California is pressuring Michael Scott to meet his quarterly quota 
+and Michael Scott is almost there he needs a thousand more dollars and 
+he comes up with the genius idea to deduct pay for the highest-paid employee at his branch beside himself 
+so how does he go about doing this identifying the person that makes the most money 
+*/
 
 SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, Salary
 FROM SQLTutorial.dbo.EmployeeDemographics
