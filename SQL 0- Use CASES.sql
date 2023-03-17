6871db1baeca7807802e2fcfb5231154d21c436a
Mustafa-Alhnuaiti <83397235+Mustafa-AlHunaiti@users.noreply.github.com>
2023-03-17 03:16:51 +0300

    Update SQL 0- Use CASES.sql

--- SQL 0- Use CASES.sql
+++ SQL 0- Use CASES.sql
@@ -17,13 +17,15 @@ ORDER BY Salary DESC
 
 
 /*
-2- use case: Robert California is pressuring Michael Scott to meet his quarterly quota 
-   	     and Michael Scott is almost there he needs a thousand more dollars and 
-             he comes up with the genius idea to deduct pay for the highest-paid employee at his branch beside himself 
-             so how does he go about doing this identifying the person that makes the most money 
+2- use case: Kevin Malone who is an accountant thinks that he may have made a mistake when looking at the average salary for our salesmen
+	     now Angela Martin is very good at SQL and so what she is gonna do is she wants to go in and calculate the average salary for our salesmen  
 */
-
-
+SELECT JobTitle, AVG(Salary) AS AverageSalary
+FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary 
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE JobTitle = 'Salesman'
+GROUP BY JobTitle
 
 
 
